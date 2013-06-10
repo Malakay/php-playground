7d4e7459d3400a7ddc260d217c282b1644cf5b65
mkh <>
2013-06-10 17:15:04 +0200

    mal was ausprobiert

--- Bootstrap-Test/index.php
+++ Bootstrap-Test/index.php
@@ -1,12 +1,32 @@
+<?php
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $randomString;
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Test Bootstrap</title>
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <table class="table-hover" align="center">
+            <?php
+            for ($x = 1; $x <= 10; $x++) {
+                echo '<tr>';
+                echo '<td>'.$x.'</td>';
+                echo '<td>'.generateRandomString().'</td>';
+                echo '</tr>';
+            }
+            ?>
+        </table>
     </body>
 </html>
